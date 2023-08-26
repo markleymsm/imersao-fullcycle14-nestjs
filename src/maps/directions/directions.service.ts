327b6719232bb6891c6ee30746c5a268fbf8355b
Markley Mendes <markley.mendes@gmail.com>
2023-08-26 16:47:23 +0000

    refactor: improve code getDirections

--- src/maps/directions/directions.service.ts
+++ src/maps/directions/directions.service.ts
@@ -14,40 +14,36 @@ export class DirectionsService {
   ) {}
 
   async getDirections(placeOriginId: string, placeDestinationId: string) {
-    try {
-      const requestParams: DirectionsRequest['params'] = {
-        origin: `place_id:${placeOriginId.replace('place_id', '')}`,
-        destination: `place_id:${placeDestinationId.replace('place_id', '')}`,
-        mode: TravelMode.driving,
-        key: this.configService.get<string>('GOOGLE_MAPS_API_KEY'),
-      };
+    const requestParams: DirectionsRequest['params'] = {
+      origin: `place_id:${placeOriginId.replace('place_id:', '')}`,
+      destination: `place_id:${placeDestinationId.replace('place_id:', '')}`,
+      mode: TravelMode.driving,
+      key: this.configService.get<string>('GOOGLE_MAPS_API_KEY'),
+    };
 
-      const { data } = await this.googleMapsClient.directions({
-        params: requestParams,
-      });
+    const { data } = await this.googleMapsClient.directions({
+      params: requestParams,
+    });
 
-      return {
-        ...data,
-        request: {
-          origin: {
-            place_id: requestParams.origin,
-            location: {
-              lat: data.routes[0].legs[0].start_location.lat,
-              lng: data.routes[0].legs[0].start_location.lng,
-            },
+    return {
+      ...data,
+      request: {
+        origin: {
+          place_id: requestParams.origin,
+          location: {
+            lat: data.routes[0].legs[0].start_location.lat,
+            lng: data.routes[0].legs[0].start_location.lng,
           },
-          destination: {
-            place_id: requestParams.destination,
-            location: {
-              lat: data.routes[0].legs[0].end_location.lat,
-              lng: data.routes[0].legs[0].end_location.lng,
-            },
-          },
-          mode: requestParams.mode,
         },
-      };
-    } catch (error) {
-      console.error(error.response.data);
-    }
+        destination: {
+          place_id: requestParams.destination,
+          location: {
+            lat: data.routes[0].legs[0].end_location.lat,
+            lng: data.routes[0].legs[0].end_location.lng,
+          },
+        },
+        mode: requestParams.mode,
+      },
+    };
   }
 }
