0c20b8216e2dff288762050ba76814b26404ca11
Markley Mendes <markley.mendes@gmail.com>
2023-08-27 00:09:15 +0000

    rafactor: improve gateway code

--- src/routes/routes/routes.gateway.ts
+++ src/routes/routes/routes.gateway.ts
@@ -1,10 +1,14 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: '*' })
 export class RoutesGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    console.log('handle message', payload);
-    return 'Hello world!';
+  @SubscribeMessage('new-points')
+  handleMessage(
+    client: Socket,
+    payload: { route_id: string; lat: number; lng: number },
+  ) {
+    client.broadcast.emit('admim-new-points', payload);
+    client.broadcast.emit(`new-point/${payload.route_id}`, payload);
   }
 }
